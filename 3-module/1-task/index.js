fead7605dc532d1404a48162cc52e0ff1bff0345
Leonid Rusakov <bengunn87@mail.ru>
2019-02-23 00:07:20 +0500

    Решение задачи 3_1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,12 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let salaryArr = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]['age'] <= age) {
+	salaryArr.push(data[i]['name'] + ', ' + data[i]['balance']);
+	};
+  };
+  return salaryArr.join('\n');
 }
-
 
