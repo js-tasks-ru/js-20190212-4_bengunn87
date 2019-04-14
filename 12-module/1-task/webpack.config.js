992c9332e8ab4c4b898b8bc7a39df0fc8bf607a7
Leonid Rusakov <bengunn87@mail.ru>
2019-04-15 00:38:46 +0500

    Решение задачи 12_1

--- 12-module/1-task/webpack.config.js
+++ 12-module/1-task/webpack.config.js
@@ -3,4 +3,10 @@ module.exports = {
      * Здесь должен быть описан ваш конфиг для сборки.
      * Для ссылки на текущую папку используйте встроенную переменную __dirname
      */
+    entry: './modules/app',
+
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname)
+    }
 };
